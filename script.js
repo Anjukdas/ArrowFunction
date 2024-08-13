88cfd86fc8c5b90a8c92ee7a5c88063c852e53a0
Anjukdas <anjukollara@gmail.com>
2024-08-13 18:29:52 +0530

    1rd  commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const processString = (str) => {
+    return str.split('').map((char, index) => `${index}${char.toUpperCase()}`).join('');
+  };
+  
+  const inputString = prompt('Enter a string:');
+  const result = processString(inputString);
+  
+  document.body.innerHTML = result;
+  
